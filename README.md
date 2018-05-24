b604bff9bfcf0c62681aea1a26401102e5063efc
Vidal Gonzalez <darkvidal@gmail.com>
2018-05-23 22:29:32 -0500

    Spreading objects for reducers

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # expensify-app
 React application for practicing purposes
+
+
+To get more information about spread operator plugin, search in Google for: babel object spread operator
--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,10 +1,72 @@
 import {createStore, combineReducers} from 'redux';
+import uuid from 'uuid';
+
+//ADD_EXPENSE
+const addExpense = (
+    {
+        description = '', 
+        note = '', 
+        amount = 0, 
+        createdAt = 0
+    } = {}) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        description,
+        note,
+        amount,
+        createdAt
+    }
+});
+
+//REMOVE_EXPENSE
+const removeExpense = ({id}) => ({
+    type: 'REMOVE_EXPENSE',    
+    id
+    
+});
+
+//EDIT_EXPENSE
+const editExpense = (id, updates) => ({
+    type: 'EDIT_EXPENSE',
+    id,
+    updates
+});
+
+const setTextFilter = (text = '') => ({
+    type: 'SET_TEXT_FILTER',
+    text
+});
+
+//SET_EXPENSE
+//SORT_BY_DATE
+//SORT_BY_AMOUNT
+//SET_START_DATE
+//SET_END_DATE
 
 //Expenses Reducer
 const expensesReducerDefaultState = [];
 
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
     switch(action.type) {
+        case 'ADD_EXPENSE': 
+            return [
+                ...state,
+                action.expense
+            ]; 
+        case 'REMOVE_EXPENSE':
+            return state.filter(({ id }) => id !== action.id);
+        case 'EDIT_EXPENSE' :
+            return state.map( (expense) => {
+                if (expense.id === action.id) {
+                    return {
+                        ...expense, 
+                        ...action.updates
+                    }
+                } else {
+                    return expense;
+                }
+            });
         default: 
             return state;
     }
@@ -20,6 +82,11 @@ const filterReducerDefaultState = {
 
 const filtersReducers = (state = filterReducerDefaultState, action) => {
     switch(action.type){
+        case 'SET_TEXT_FILTER':
+            return {
+                ...state,
+                text: action.text
+            };
         default:
             return state;
     }
@@ -33,7 +100,21 @@ const store = createStore(
     })
 );
 
-console.log(store.getState());
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 5 }));
+
+console.log(expenseOne);
+
+store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+
+store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
+
+store.dispatch(setTextFilter('rent'));
+store.dispatch(setTextFilter());
 
 const demoState = {
     expenses: [{
