91120cb842a11fd6db87a39d2fd1240b50240828
darkvidal <darkvidal@gmail.com>
2018-05-25 18:17:03 -0400

    Connecting Store and Component with React-Redux

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import getVisibleExpenses from './selectors/expenses';
@@ -12,10 +13,20 @@ const store = configureStore();
 //store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: 1000 }));
 store.dispatch(addExpense({ description: 'Water bill'}));
 store.dispatch(addExpense({ description: 'Gas bill'}));
-store.dispatch(setTextFilter('water'))
+store.dispatch(setTextFilter('water'));
+
+setTimeOut(() => {
+    store.dispatch(setTextFilter('rent'));
+});
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters );
 console.log(visibleExpenses);
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const jsx = (
+  <Provider store={store}>
+    <AppRouter  />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ExpenseList from './ExpenseList';
 
 const ExpenseDashboardPage = () => (
   <div>
-    This is from my dashboard component
+    <ExpenseList />
   </div>
 );
 
