5d4db4ca337152f74dd9d680bd41d0f4763e3723
darkvidal <darkvidal@gmail.com>
2018-05-31 11:28:29 -0400

    Mocking Libraries with Jest

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 //store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: 1000 }));
