d3e3081be592faae555a60e3a09f3b128d748378
Vidal Gonzalez <darkvidal@gmail.com>
2018-05-30 23:01:29 -0500

    Snapshot Testing with Dynamics Components

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import {connect } from 'react-redux';
+import { connect } from 'react-redux';
 import ExpenseListItem from './ExpenseListItem';
 import selectExpenses from '../selectors/expenses';
 
-const ExpenseList = (props) => (
+export const ExpenseList = (props) => (
   <div>
-    <h1>Expense List</h1>    
+    <h1>Expense List</h1>
     {
-      props.expenses.map((expense) => {
-        return  <ExpenseListItem key={expense.id} {...expense} />
-      })
-    }  
-  </div>  
+      props.expenses.length === 0 ? (
+        <p>No expenses</p>
+      ) : (
+          props.expenses.map((expense) => {
+            return <ExpenseListItem key={expense.id} {...expense} />
+          })
+        )
+    }    
+  </div>
 );
 
 
 const mapStateToProps = (state) => {
-    return {
-        expenses: selectExpenses(state.expenses, state.filters)
-    };
+  return {
+    expenses: selectExpenses(state.expenses, state.filters)
+  };
 }
 
 export default connect(mapStateToProps)(ExpenseList);
