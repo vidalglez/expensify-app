0fbb75c3647e8772eeea8b56bb8b4cae3ced7e6a
Vidal Gonzalez <darkvidal@gmail.com>
2018-06-27 16:28:40 -0500

    Styling Header, Buttons and  Summary Area

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,9 +5,9 @@ import { startLogin } from '../actions/auth';
 export const LoginPage = ({ startLogin }) => (
     <div className="box-layout">
         <div className="box-layout__box">
-            <h1 className="box-layout__title">Expensify App</h1>
+            <h1 className="box-layout__title">Expensify</h1>
             <p>Setting your expenses in total control</p>
-            <button onClick={startLogin} >Login</button>
+            <button onClick={startLogin} className="button" >Login with Google</button>
         </div>
     </div>
 );
